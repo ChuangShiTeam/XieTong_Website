3ef1eabf56f1f55a45062a361f3236ec8fa6d41c
nowui <nowui@163.com>
2017-11-06 21:39:08 +0800

    修改

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+    list: []
+}
+
+function product(state = initialState, action) {
+    switch (action.type) {
+        case 'index':
+            return Object.assign({}, state, action.data);
+
+        default :
+            return state;
+    }
+}
+
+export default product;
