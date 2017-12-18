6252a9ebe67091de518b4597a3fbc2a1d92939e9
NowUI <nowui@163.com>
2017-12-18 15:33:37 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routers from './Routers';
 
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (prefix) {
+        return this.slice(0, prefix.length) === prefix;
+    };
+}
+
 document.getElementById("loading").remove();
 
 ReactDOM.render(
